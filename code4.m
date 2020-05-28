cbe59aa4a3882a73fecfe9d772cb55fe0c30443b
Rohan Urdhwareshe <rurdhwareshe@gmail.com>
2020-05-28 22:12:47 +0530

    Update code4.m

--- code4.m
+++ code4.m
@@ -1,33 +1,33 @@
 clc
 clear all;
-x=input('Please the number upto which you want to have prime numbers:');
-i=3;
-flag=0;
-prime(1)=2;
-k=2;
+x = input('Please the number upto which you want to have prime numbers:');
+test_no = 3;
+flag = 0;
+prime(1) = 2;
+cnt = 2;
 tic
-while i<=x
-    sqrt_i=sqrt(i);
-    j=1;
-    for j=2:1:length(prime)
-        if j>=sqrt_i
+while test_no <= x
+    sqrt_i = sqrt(test_no);
+    j = 1;
+    for j = 2:1:length(prime)
+        if j >= sqrt_i
             break;
         else
-        if(mod(i,prime(j))==0)
-            flag=1;
-            break;
-        else
-            continue;
-        end
+            if (mod(test_no,prime(j)) == 0)
+                flag = 1;
+                break;
+            else
+                continue;
+            end
         end
     end
-    if flag==0
-        prime(k)=i;
-        k=k+1;
+    if flag == 0
+        prime(cnt) = test_no;
+        cnt = cnt+1;
     else
-        flag=0;
+        flag = 0;
     end
-    i=i+2;
-end       
+    test_no = test_no+2;
+end
 toc
-prime;
+prime
